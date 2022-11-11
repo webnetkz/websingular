a43598c799841a91ed7275132369b5c77e5d2e07
webnetkz <dbqqbq@gmail.com>
2022-11-11 17:01:18 +0600

    Первая бета минифицирует СИ подобный синтаксис

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding=utf-8           # -*- coding: utf-8 -*-
 from jsmin import jsmin
 import os
+import re
 
 
 
@@ -8,34 +9,29 @@ def minifyC(nameJSFile):
 
   with open(nameJSFile) as js_file:
     
-    minifiedJSCode = jsmin(js_file.read(), quote_chars="'\"`")
-    
-    newJSFile = open(nameJSFile, "w")
-    newJSFile.write(minifiedJSCode)
-    newJSFile.close()
+    try:
+      minifiedJSCode = jsmin(js_file.read(), quote_chars="'\"`")
+      
+      newJSFile = open(nameJSFile, "w")
+      newJSFile.write(minifiedJSCode)
+      newJSFile.close()
+    except:
+      print("На этом файле произошла ошибка обработки")
+      print("#"*50)
     
 def getAllFiles(folder):
   tree = list(os.walk(folder))
   
   for i in tree:
-
-    # Игнорирует не нужные кталоги
-    gitFolder = folder+"\.git"
-
-    if gitFolder in i[0]:
-      del i
-
-    # Удаляет пустые кталоги
-    elif (not i[1]) & (not i[2]):
-      del i 
-
-    else:
-      print("Processing folder: ", i[0])
-      for x in i[2]:
-        pathToFile = i[0]+"\\"+x
-        print("Start compression :", pathToFile)
-        minifyC(pathToFile)
-        print("Finish compression")
+    for x in i[2]:
+      if x[0] != ".":
+        resultFile = x.split(".")
+        if len(resultFile) == 2:
+          ef = resultFile[1]
+          if (ef == "js") | (ef == "php") | (ef == "css"):
+            print(i[0]+"\\"+x)
+            minifyC(i[0]+"\\"+x)
+        
 
 if __name__ == '__main__':
-  getAllFiles("./pwa/app")
+  getAllFiles("pwa")
